63ffe8a47b7398fd2bd651db49f48bb1e964aafc
arinaleech <ndjdn6062@gmail.com>
2025-01-17 15:39:12 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -41,3 +41,4 @@ class Database:
     async def get_thumbnail(self, id):
         user = await self.col.find_one({'id': int(id)})
         return user.get('thumbnail', None)
+db = Database(Config.DATABASE_URL, "UploadLinkToFileBot")
